fb5496dbd54b8516727e7654c790e4e7abdcfb70
julian <6efilm.infobibliotecas@gmail.com>
2021-09-09 19:13:01 -0500

    remove .serverless

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from mangum import Mangum
+import os
+
+stage = os.environ.get('STAGE', None)
+openapi_prefix = f"/{stage}" if stage else "/"
 
 
-app = FastAPI(title="serverless-app")
+app = FastAPI(title="serverless-app", openapi_prefix=openapi_prefix)
 
 
 class Item(BaseModel):
@@ -12,3 +17,6 @@ class Item(BaseModel):
 @app.post("/webhook")
 async def webhook(item: Item):
     return item
+
+
+handler = Mangum(app)
