78500c4155691056784273b546529ca24133b306
julian <6efilm.infobibliotecas@gmail.com>
2021-09-12 21:08:35 -0500

    added request object

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from mangum import Mangum
 from datetime import datetime
+from typing import Optional
+
 
 import os
 
@@ -13,18 +15,19 @@ app = FastAPI(title="serverless-app", openapi_prefix=openapi_prefix)
 
 
 class Item(BaseModel):
-    dataProduto: str
-    macroProcesso: str
-    nome: str
-    periodicidade: datetime
-    periodicidadFinal: datetime
-    processo: str
-    url: str
+    dataProduto: Optional[str] = None
+    macroProcesso: Optional[str] = None
+    nome: Optional[str] = None
+    periodicidade: Optional[datetime] = None
+    periodicidadFinal: Optional[datetime] = None
+    processo: Optional[str] = None
+    url: Optional[str] = None
 
 
 @app.post("/webhook")
-async def webhook(item: Item):
+async def webhook(request: Request, item: Item):
     print(item)
+    print(await request.json())
     return item
 
 
