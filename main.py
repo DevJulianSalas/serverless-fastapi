a7ca3c03f01b6bcb554d8e16cd5c80a9a116fa96
julian <6efilm.infobibliotecas@gmail.com>
2021-09-12 20:51:36 -0500

    added types to Item class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from mangum import Mangum
+from datetime import datetime
+
 import os
 
 stage = os.environ.get('STAGE', None)
@@ -11,11 +13,18 @@ app = FastAPI(title="serverless-app", openapi_prefix=openapi_prefix)
 
 
 class Item(BaseModel):
-    temp: float
+    dataProduto: str
+    macroProcesso: str
+    nome: str
+    periodicidade: datetime
+    periodicidadFinal: datetime
+    processo: str
+    url: str
 
 
 @app.post("/webhook")
 async def webhook(item: Item):
+    print(item)
     return item
 
 
