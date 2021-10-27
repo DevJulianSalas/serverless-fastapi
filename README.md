bf2ade64466cef5c6089da8c5efb25707fa59b44
julian <6efilm.infobibliotecas@gmail.com>
2021-10-27 10:03:09 -0500

    added ep to download zip

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Tinny example of how to run a webhook using fast-api with serverless framework.
 # How to execute
 To execute run the below command
 
-```sls deploy --aws-profile=name_profile --state=staging```
+```sls deploy --aws-profile=name_profile --stage=staging```
+
+
+run server locally
+```uvicorn main:app --reload```
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
-from fastapi import FastAPI, Request
+import sys
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from mangum import Mangum
 from datetime import datetime
 from typing import Optional
 import boto3
 import json
+import pathlib
+import requests
 
 
 # Global Variables
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('ons_webhook')
 
 
@@ -49,6 +53,18 @@ async def webhook(item: Item):
     response = add_item(item.dict())
     print(response)
     return item
+
+
+@app.get("/download_zip")
+async def download_zip():
+    url = 'https://apps08.ons.org.br/ONS.Sintegre.Proxy/webhook?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVUkwiOiJodHRwczovL3NpbnRlZ3JlLm9ucy5vcmcuYnIvc2l0ZXMvOS8xMy84Mi9Qcm9kdXRvcy8yMzgvTW9kZWxvc19DaHV2YV9WYXphb18yMDIxMTAyNi56aXAiLCJ1c2VybmFtZSI6InJpY2FyZG9AaHVlenRlbGVjb20uY29tLmJyIiwibm9tZVByb2R1dG8iOiJBcnF1aXZvcyBkb3MgbW9kZWxvcyBkZSBwcmV2aXPDo28gZGUgdmF6w7VlcyBkacOhcmlhcyAtIFBEUCIsIklzRmlsZSI6IlRydWUiLCJpc3MiOiJodHRwOi8vbG9jYWwub25zLm9yZy5iciIsImF1ZCI6Imh0dHA6Ly9sb2NhbC5vbnMub3JnLmJyIiwiZXhwIjoxNjM1Mzc5MTAzLCJuYmYiOjE2MzUyOTI0NjN9.xHZ8wQA0ZxVXBvEmJhWf_Dtd8y_wzwm7N8OgB8W6-a4'
+    r = requests.get(url, stream=True)
+    current_directory = pathlib.Path(__file__).parent.resolve()
+    full_path_file = os.path.join(current_directory, 'ons.zip')
+    with open(full_path_file, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
+    return FileResponse(full_path_file, filename='Arquivos dos modelos de previsão de vazões diárias - PDP.zip')
 
 
 handler = Mangum(app)
