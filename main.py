4ec121e9d73f91d746148cc667c8abb43724862d
julian <6efilm.infobibliotecas@gmail.com>
2021-11-01 13:14:17 -0500

    put better msg responses

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import os
-import sys
-from fastapi import FastAPI, Request, File, UploadFile
-from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.gzip import GZipMiddleware
 from pydantic import BaseModel
 from mangum import Mangum
 from datetime import datetime
-from typing import Optional
 import boto3
-import json
-import pathlib
 import requests
 
 
@@ -22,6 +19,9 @@ root_path = f"/{stage}" if stage else "/"
 
 
 app = FastAPI(title="serverless-app", root_path=root_path)
+
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
 def add_item(item):
@@ -45,22 +45,26 @@ def get_url_nome(key):
 
 
 def generate_stream_response(rsp, filename):
-    response = StreamingResponse(rsp.iter_content(
-        chunk_size=1000), media_type="application/x-zip-compressed")
-    response.headers["Content-Disposition"] = f'attachment; filename={filename}.zip'
-    return response
+    return StreamingResponse(
+        rsp.iter_content(chunk_size=1000),
+        media_type="application/x-zip-compressed",
+        headers={
+            "Content-Disposition": f"attachment; filename={filename}.zip",
+            "Content-Encoding": "gzip"
+        }
+    )
 
 
-def perform_http_strem(url):
+def perform_http_stream(url):
     try:
         rsp = requests.get(url, stream=True)
         if rsp.status_code == 200:
             return generate_stream_response(rsp, 'Arquivos dos modelos de previsão de vazões diárias - PDP')
         else:
-            return {'msg': f'oops could not process the request status codei is : {rsp.status_code} url used {url}'}
+            return {'msg': f'oops could not process the request status code is : {rsp.status_code} url used {url}'}
     except requests.exceptions.HTTPError as e:
         print(e)
-        return {'msg': 'oops a error has ocurred check logs it semees be httpError'}
+        return {'msg': 'oops an error has ocurred check logs'}
 
 
 class Item(BaseModel):
@@ -85,7 +89,7 @@ def satellite_precipitation_history_zip():
     url = get_url_nome(key)
     if not url:
         return {'msg': f'error, failed try to get url from {key} in dynamodb'}
-    return perform_http_strem(url)
+    return perform_http_stream(url)
 
 
 @app.get("/hydraulic_operation_reports")
@@ -94,7 +98,7 @@ def hydraulic_operation_reports():
     url = get_url_nome(key)
     if not url:
         return {'msg': f'error, failed try to get url from {key} in dynamodb'}
-    return perform_http_strem(url)
+    return perform_http_stream(url)
 
 
 @app.get("/daily_flow_forecast_zip")
@@ -103,7 +107,7 @@ def daily_flow_forecast_zip():
     url = get_url_nome(key)
     if not url:
         return {'msg': f'error, failed try to get url from {key} in dynamodb'}
-    return perform_http_strem(url)
+    return perform_http_stream(url)
 
 
 @app.get("/entry_exit_dectks_dessem_zip")
@@ -112,17 +116,13 @@ def entry_exit_dectks_dessem_zip():
     url = get_url_nome(key)
     if not url:
         return {'msg': f'error, failed try to get url from {key} in dynamodb'}
-    return perform_http_strem(url)
+    return perform_http_stream(url)
 
 
 handler = Mangum(app)
 
 
 def handler(event, context):
-    print(event, context)
     asgi_handler = Mangum(app)
-    print('*****')
-    print(asgi_handler)
-    print('*****')
     response = asgi_handler(event, context)
     return response
